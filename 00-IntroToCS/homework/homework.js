9b9ce183011b630da739fa6e82eb23905d7a2f8c
Kosovomba <santiagojavierlevy@gmail.com>
2022-04-11 19:24:19 -0300

    homework IntroToCs

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,38 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let numDecimal = 0
+  let m = 0
+  let i = 0
+  let num2 = num
+  for(i = 0; i < num.length; i++) {
+    m = Math.floor(num2/Math.pow(10,num.length - 1 - i))
+    numDecimal += m*Math.pow(2,num.length - 1 - i)
+    num2 = num2 - Math.pow(10,num.length - 1 - i)
+  }
+  return numDecimal
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  if (num == 0) {
+    return num
+  }
+  else {
+    const num3 = ''
+    let numBinario = 0
+    let num2 = num
+    let i = 0
+    while (num2 - Math.pow(2,i) >= 0) {
+      let i = 0
+      while (num2 - Math.pow(2,i) >= 0) {
+        i = i + 1
+      }
+      numBinario += Math.pow(10,i - 1)
+      num2 = num2 - Math.pow(2,i - 1)
+    }
+    return numBinario + num3
+  }
 }
 
 
